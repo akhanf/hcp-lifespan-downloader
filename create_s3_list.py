f3ba6c25a6ce54382ee3c3b14395b5e25c5de5a0
akhanf <alik@robarts.ca>
2021-09-23 10:03:01 -0400

    uncommitted changes

--- create_s3_list.py
+++ create_s3_list.py
@@ -8,8 +8,8 @@ df = pd.read_table(snakemake.input.data_manifest,sep='\t')
 df_subj = df[df['associated_file'].str.contains(snakemake.wildcards.subject)]
     
 #get dataset id for the job
-dataset_id = df_mri.loc[df_mri['job_name']==snakemake.params.package_name,'dataset_id'].unique()
+dataset_id = df_mri.loc[df_mri['image_collection_name']==snakemake.params.package_name,'dataset_id'].unique()
     
 #get files for this dataset and write to file
-df_subj.loc[df['dataset_id'].isin(dataset_id),'associated_file'].to_csv(snakemake.output.txt_file,header=False,index=False)
+df_subj.loc[df['dataset_id'].isin(dataset_id.astype('str')),'associated_file'].to_csv(snakemake.output.txt_file,header=False,index=False)
 
